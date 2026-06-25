c76f84ee8b39a7ed2b2cbd7c33d173100cc76ca3
NguyenVo-09 <nguyenvo09@example.com>
2026-06-25 16:09:14 +0700

    Remove healthcare_map folder (separate project)

--- healthcare_map/lib/services/device_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:async';
-import '../models/device_data.dart';
-import '../models/fall_event.dart';
-
-abstract class DeviceRepository {
-  Stream<DeviceData> get deviceStream;
-  Future<List<FallEvent>> getFallHistory();
-}
